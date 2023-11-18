f341305ec27fdc25ce9d6e6cb321047071060df9
Mieciek <mateusz.muzyka@katowice.zdz.katowice.pl>
2023-11-18 11:52:44 +0100

    Update script.js

--- kalkulator-win/script.js
+++ kalkulator-win/script.js
@@ -11,6 +11,14 @@ let P = null;
 let g = null;
 let a = null;
 
+
+let d = null;
+let dd = null;
+let o = null;
+let pp = null;
+let il = null;
+let pr = null;
+
 const ekran = document.querySelector('#ekran')
 const guz = document.querySelectorAll(".but")
 	console.log(guz)
@@ -64,42 +72,83 @@ for(let i=0; i< guz.length; i++){
 			case "+":
 				ekran.value = ("+")
 				
-				DZ = parseInt(L1) + parseInt(L2);
+				d = 1;
+				
 				g = 0;
 				a = 1;
 			break;		
 
 			case "-":
 				ekran.value = ("-")
-				DZ = L1 - L2;
+				o = 1;
 				a = 1;
 			break;		
 
 			case "*":
 				ekran.value = ("*")
-				DZ = L1 * L2;
+				il = 1;
 				a = 1;
 			break;		
 
 			case "/":
 				ekran.value = ("/")
-				DZ = L1 / L2;
+				dd = 1;
 				a = 1;
 			break;		
 
 			case "%":
 				
 				ekran.value = ("%")
-				DZ = L1 % L2;
+				pr = 1;
 				a = 1;
 			break;	
 			
 			case "=":
 				
-				P = DZ
-				ekran.value = (P)
 				
 				
+				
+				if(pp == 1){
+					if(L1 != null || L1 == 0){
+					DZ = Math.sqrt(L1)
+					console.log(L1)
+				console.log(DZ)}
+				else {
+					DZ = Math.sqrt(L2)
+					console.log(L2)
+					console.log(DZ)
+				}
+					
+			   }
+				if(d == 1){
+					DZ = parseInt(L1) + parseInt(L2)
+				}
+				
+				if(dd == 1){
+					DZ = L1 / L2
+				}
+				
+				if(o == 1){
+					DZ = L1 - L2;
+				}
+				
+				if(il == 1){
+					DZ = L1 * L2;				
+				}
+				
+				if(pr == 1){
+					pr = 0
+				}
+				
+				ekran.value = (DZ)
+				pp = null;
+				o = null;
+				d = null;
+				dd = null;
+				pr = null;
+				il = null;
+				L1 = null;
+				L2 = null;
 			break;		
 
 			case "C/CE":
@@ -111,20 +160,21 @@ for(let i=0; i< guz.length; i++){
 			
 			case "MRC":
 				a = 0;
+				pp = null;
+				o = null;
+				d = null;
+				dd = null;
+				pr = null;
+				il = null;
 			break;
 			
 
 			case "√":
 				ekran.value = ("√")
-				if(L1 != null || L1 == 0){
-					DZ = Math.sqrt(L1)
-					console.log(L1)
-				console.log(DZ)}
-				else {
-					DZ = Math.sqrt(L2)
-					console.log(L2)
-					console.log(DZ)
-		}}
+				
+				pp = 1;
+				
+		}
 		
 	}
 	
