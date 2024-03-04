d07b2cca0b719da3b87a714cbc07c24c462db0c1
Mieciek <mateusz.muzyka@katowice.zdz.katowice.pl>
2024-03-04 05:51:00 +0100

    makumba

--- wisielec/js.js
+++ wisielec/js.js
@@ -2,8 +2,32 @@ const plansza = document.getElementById('plansza');
 const fraza = document.getElementById('fraza');
 const slowa = document.querySelectorAll('.slowo')
 const ekran = document.getElementById('ekran')
-plansza.addEventListener("keypress", sprawdz);
-console.log(slowa)
+let p = 0;
+let delta = true;
+let klikniety = null;
+
+let haslo = [
+  "banan",
+  "kiwi"
+];
+let b = haslo.length - 1;
+console.log(b)
+let losowe = Math.round(Math.random() * b);
+console.log(haslo[losowe])
+console.log(losowe)
+
+
+
+let wylosowany = haslo[losowe]
+let weto = String(wylosowany.length)
+for(let a = 0; a < weto; a++){
+  console.log(wylosowany.charAt(a))
+  slowa[a].style.display = "flex";
+  slowa[a].classList.add('slowa2');
+}
+
+const slowa2 = document.querySelectorAll('.slowa2')
+
 let imgs = [  
   "assets/01.png",
   "assets/02.png",
@@ -16,18 +40,43 @@ let imgs = [
   "assets/09.png",
   "assets/10.png"
 ];
+
+for(let q = 0; q < slowa2.length; q++){
+  console.log(slowa2.length)
+  slowa2[q].addEventListener('input', sprawdz)
+}
+
+
 let licznik = null;
 function sprawdz() {
-    fraza.style.backgroundColor = "aqua";
-    for (let i = 0; i < slowa.length; i++) {
-        slowa[i].style.backgroundColor = "aqua";
-      }
-      console.log(slowa)
+    // fraza.style.backgroundColor = "aqua";
+    // for (let i = 0; i < slowa2.length; i++) {
+    //     slowa2[i].style.backgroundColor = "aqua";
+    //   }
       
+
+        
+      // if(delta == false){
       licznik++
-      
+      if(licznik < imgs.length){
       const HP = document.createElement('img')
       HP.src = imgs[licznik]
       HP.classList.add('wisielec');
-      ekran.appendChild(HP)
+      ekran.appendChild(HP)}else{
+        endgame();
+      }
+    // }
+
     }
+
+    
+function endgame(){
+  if(p == 0){
+    alert('Koniec gry! przegrales!')
+    p = 1;
+  }
+  for (let i = 0; i < slowa2.length; i++) {
+    slowa2[i].readOnly = true;
+  }
+}
+
