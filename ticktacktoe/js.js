5f065affc666b107e99b63d4d853e8dbe9012007
Mieciek <131866876+Mateusz-Muzyka@users.noreply.github.com>
2024-03-26 12:35:01 +0100

    Add files via upload

--- ticktacktoe/js.js
+++ ticktacktoe/js.js
@@ -1,12 +1,24 @@
 
 const tablica = document.querySelectorAll("th")
+const table = document.getElementById("table")
+const startscreen = document.getElementById("startscreen")
+const btn1 = document.getElementById("btnStart")
+const text = document.getElementById("text")
+
+let schema = [
+    "1,1,1,0,0,0,0,0,0",
+
+]
+
 let i = 0;
 let p = 0;
-let osoba = 1;
+let osoba = true;
+btn1.addEventListener('click',start);
 const kolejka = document.getElementById('kolejka');
 console.log(tablica);
 for(i; i<tablica.length; i++){
     tablica[i].addEventListener('click', change)
+    tablica[i].addEventListener('click', points)
 
 }
 function change(){
@@ -20,6 +32,20 @@ function change(){
         kolejka.innerText = 1;
         console.log('2');
     }
-    
-    switch()
+}
+
+function start(){
+    startscreen.style.display = "none";
+    table.style.display = "block";
+    text.style.display = "block";
+    btn1.style.display = "none";
+}
+
+
+function gameloop(){
+
+}
+
+function points(event){
+    console.log(event.target.id)
 }
