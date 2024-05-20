7d552fdc044ba5684026f240980df892f30ccb69
Mieciek <131866876+Mateusz-Muzyka@users.noreply.github.com>
2024-05-20 05:43:02 +0200

    Add files via upload

---
+++ pulser/js.js
@@ -0,0 +1,43 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "red";
+
+let PosX = (canvas.width/2)-5;
+let PosY = (canvas.height/2)-10;
+
+let Multiplier = 0.8;
+
+
+let width = 10;
+let height = 10;
+
+initializer();
+
+function initializer(){
+    window.requestAnimationFrame(drawing)
+    
+}
+
+function drawing(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    
+    ctx.fillRect(PosX, PosY, width,height);
+
+    console.log(PosY)
+    width += Multiplier;
+    height += Multiplier;
+    PosX -= Multiplier/2;
+    PosY -= Multiplier/2;    
+    if(height >= canvas.height/2){  
+        Multiplier = Multiplier*-1
+      }
+    if(height <= 10){
+        Multiplier = 1;
+    }
+    
+
+
+
+
+  window.requestAnimationFrame(drawing)
+}
