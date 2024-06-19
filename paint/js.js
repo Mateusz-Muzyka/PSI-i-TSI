2e88f997adb79cbcc110be937a72cbf513b8ca19
Mieciek <mateusz.muzyka@katowice.zdz.katowice.pl>
2024-06-19 20:46:35 +0200

    161516566

--- paint/js.js
+++ paint/js.js
@@ -3,16 +3,25 @@ const reset = document.getElementById("button");
 const ctx = canvas.getContext("2d");
 
 let LevelSave = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [2,2,1,2,2,2,2,2,1,2,2,2,2,2,2],
+    [2,2,1,2,2,2,2,2,1,2,2,2,2,2,2],
+    [2,2,1,2,2,2,2,2,1,2,2,2,2,2,2],
+    [2,2,1,2,2,2,2,2,1,2,2,2,2,2,2],
+    [2,2,1,2,2,2,2,2,1,2,2,2,2,2,2],
     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    [2,2,2,2,2,1,2,2,2,2,2,2,2,2,2],
+    [2,2,2,2,2,1,2,2,2,2,2,2,2,2,2]
 ]
-const Saver = document.getElementById("val")
+
+let UserSave = []
+
+     for(let i = 0;i<LevelSave.length;i++){
+         UserSave[i] = new Array(LevelSave[1].length)
+     }
+
+
+
+
 const importBut = document.getElementById("import")
 
 importBut.addEventListener("click",impor)
@@ -35,6 +44,8 @@ const black = document.getElementById("black")
 
 const dirt = document.getElementById("dirt")
 const star = document.getElementById("star")
+const cobble = document.getElementById("cobble")
+const grass = document.getElementById("grass")
 
 
 let size = 40;
@@ -53,6 +64,8 @@ panels.forEach(button => button.addEventListener('click', ChangeColor));
 
 dirt.addEventListener('click', tools)
 star.addEventListener('click', tools)
+cobble.addEventListener('click', tools)
+grass.addEventListener('click', tools)
 
 reset.addEventListener("click", ()=>{    ctx.clearRect(0,0,canvas.width,canvas.height)})
 canvas.addEventListener("mousedown", (e)=>{  draw = 1;});
@@ -67,6 +80,7 @@ canvas.addEventListener("mousemove", (e) => {
             ctx.fillRect(PosX, PosY, width,height);
         }
         if(penter == "dirt"){
+            console.log(snapnap, snapnap2)
             const img = new Image();
             img.onload = () => {
               ctx.drawImage(img, snapnap,snapnap2,size,size)
@@ -80,6 +94,24 @@ canvas.addEventListener("mousemove", (e) => {
         
             };
             img.src = "sprite2.png";
+        }
+        if(penter == "cobble"){
+            const img = new Image();
+            img.onload = () => {
+              ctx.drawImage(img, snapnap,snapnap2,size,size)
+        
+            };
+            img.src = "sprite3jpeg.jpeg";
+            
+        }
+        if(penter == "grass"){
+            const img = new Image();
+            img.onload = () => {
+              ctx.drawImage(img, snapnap,snapnap2,size,size)
+        
+            };
+            img.src = "sprite4.png";
+            
         }
     }
 
@@ -103,6 +135,7 @@ function initializer(){
 }
 function drawing(){  
     window.requestAnimationFrame(drawing)  
+    
     width = swipe.value/5
     height = swipe.value/5
 
@@ -111,27 +144,65 @@ function drawing(){
 
 
 function impor(){
-    let TPx
-    let TPy
+    // for(let i = 0;i<LevelSave.length;i++){
+    //     UserSave[i] = new Array(LevelSave[1].length)
+    //     for(let q = 0;q<LevelSave[1].length;q++){
+    //         UserSave[i][q] = Math.round(Math.random() * 3); ;
+    //     }
+    //     console.log(UserSave)
+    // }
+
+  
+    let input2U = document.getElementsByName("QuickSave") 
+
+    for(let i=0;i<UserSave.length;i++){
+        let quick = input2U[i].value
+        quick = Array.from(quick)
+        for(let q=0;q<LevelSave[0].length;q++){
+            UserSave[i][q] = quick[q]
+        }
+    }
+    console.log(UserSave)
+    for(let i = 0;i<LevelSave.length;i++){
+        for(let q = 0;q<LevelSave[1].length;q++){
+            LevelSave[i][q] = UserSave[i][q]
+        }
+    }
+    console.log( LevelSave)
+
+
 for(let i = 0; i<8;i++){
     for(let q = 0; q<15;q++){
         let temp = LevelSave[i][q];
-         TPx = snapnap/size;
-         TPy = snapnap2/size;
-         console.log(snapnap,size)
+        
         switch(temp){
-            case 0:
-
-            break;
+            case '0':
             
-            case 1:
+                break;
+                
+                case '1':
                 const img = new Image();
                 img.onload = () => {
-                  ctx.drawImage(img, TPx*size*q,TPy*size*q,size,size)
+                  ctx.drawImage(img, size*q,size*i,size,size)
                 };
                 img.src = "sprite1.jpg";
             break;
-
+                
+                case '2':
+                const img2 = new Image();
+                img2.onload = () => {
+                  ctx.drawImage(img2, size*q,size*i,size,size)
+                }
+                img2.src = "sprite3jpeg.jpeg";
+            break;
+                case '3':
+                const img3 = new Image();
+                img3.onload = () => {
+                  ctx.drawImage(img3, size*q,size*i,size,size)
+                }
+                img3.src = "sprite4.png";
+            break;
+        
         }
     }
 }
@@ -139,6 +210,7 @@ for(let i = 0; i<8;i++){
 
 function tools(){
     clicked = this.id;
+    console.log(this.id)
     switch(clicked){
         case "pen":
             penA = true;
@@ -153,6 +225,12 @@ function tools(){
         break;
         case "star":
             penter = "star"
+        break;
+        case "cobble":
+            penter = "cobble" 
+        break;
+        case "grass":
+            penter = "grass" 
         break;
     }
 }
@@ -196,4 +274,7 @@ function ChangeColor(){
   }
 }
 
+function inputer(){
+
+}
 
